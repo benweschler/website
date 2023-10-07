01d5e104f81ed0d2688819beb427d1e63e5aa78c
Ben Weschler <benjaminweschler@gmail.com>
2023-10-07 03:42:41 -0700

    Updated under construction page with greeting.

--- lib/screens/under_contruction_screen.dart
+++ lib/screens/under_contruction_screen.dart
@@ -2,9 +2,13 @@ import 'dart:convert';
 
 import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:website/style/theme.dart';
+import 'package:website/utils/http_utils.dart';
 import 'package:website/widgets/responsive_button.dart';
 
+// TODO: this is very long and messy but it doesn't matter because it's readable enough and it's GOING AWAY SOON!!.
 class UnderConstructionScreen extends StatefulWidget {
   final VoidCallback validateAdmin;
 
@@ -17,7 +21,8 @@ class UnderConstructionScreen extends StatefulWidget {
 
 class _UnderConstructionScreenState extends State<UnderConstructionScreen> {
   // HAHA you sneaky sneaker. Checking the source code to try and get in smh...
-  // Try breaking SHA256 n00b :)
+  // Wait until the site is ready!!! I promise it'll worth the wait.
+  // Props for getting here, though :).
   final _magicWordHash =
       'd2a27049b0b146881e0c7bb4e760704ee9689f5c21c3f6279c6529fb66cca8ac';
   final _controller = TextEditingController();
@@ -26,6 +31,14 @@ class _UnderConstructionScreenState extends State<UnderConstructionScreen> {
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+
+  void _downloadResume() async {
+    final bytes = await rootBundle.load('assets/resume.pdf');
+    downloadFileFromBytes(
+      bytes.buffer.asUint8List(),
+      'Benjamin Weschler Resume.pdf',
+    );
   }
 
   void _validate() {
@@ -46,7 +59,17 @@ class _UnderConstructionScreenState extends State<UnderConstructionScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text(
-                'This website is under construction and is waiting for DNS propagation, but it won\'t be for long! Check back the morning of October 7th.',
+                'This website is under construction and is waiting for DNS propagation and certificate provisioning, but it won\'t be long! Check back in the late afternoon or evening of October 7th.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  height: 1.25,
+                  letterSpacing: 1.33,
+                ),
+              ),
+              const SizedBox(height: 50),
+              const Text(
+                'Are you from Nova for Good? Hey!! Thanks for stopping by :). I\'m SO excited to apply and show you what I have here, so PLEASE come back soon and check out my portfolio once everything is up and running — I promise it won\'t be too long. In the meantime, you can check out my GitHub or download my resumé by clicking below.',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 16,
@@ -54,15 +77,118 @@ class _UnderConstructionScreenState extends State<UnderConstructionScreen> {
                   letterSpacing: 1.33,
                 ),
               ),
-              const SizedBox(height: 50),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ResponsiveButton(
+                    onClicked: _downloadResume,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(7),
+                        color: AppColors.of(context).headerColor,
+                      ),
+                      child: const Text(
+                        'Download Resumé',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          height: 1.25,
+                          letterSpacing: 1.33,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 30),
+                  ResponsiveButton(
+                    onClicked: () => launchUrl(
+                      Uri.parse('https://www.github.com/benweschler'),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(7),
+                        color: AppColors.of(context).headerColor,
+                      ),
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/github-invertocat-logo.png',
+                            color: Colors.black,
+                            height: 22,
+                          ),
+                          const SizedBox(width: 10),
+                          const Text(
+                            'GitHub',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              height: 1.25,
+                              letterSpacing: 1.33,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 200),
               const Text(
-                'Are you an admin and want to get in? What\'s the magic word then...',
+                'Are you Ben and want to get in? What\'s the magic word then...',
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 14,
+                  fontSize: 16,
                   height: 1.25,
                   letterSpacing: 1.33,
                 ),
+              ),
+              const SizedBox(height: 5),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'Maybe you can even try to find it in the ',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 12,
+                      height: 1.25,
+                      letterSpacing: 1.33,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () => launchUrl(
+                      Uri.parse('https://github.com/benweschler/website/blob/main/lib/screens/under_construction_page.dart'),
+                    ),
+                    child: const Text(
+                      'source code',
+                      style: TextStyle(
+                        fontSize: 12,
+                        height: 1.25,
+                        letterSpacing: 1.33,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                  const Text(
+                    '?',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 12,
+                      height: 1.25,
+                      letterSpacing: 1.33,
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 10),
               SizedBox(
