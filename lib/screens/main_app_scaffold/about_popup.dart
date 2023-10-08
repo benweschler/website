f6daef0a1fb3abda200cc749e417752cd40ad65f
Ben Weschler <benjaminweschler@gmail.com>
2023-10-08 10:36:01 -0700

    Improved mobile view of about popup.

--- lib/screens/main_app_scaffold/about_popup.dart
+++ lib/screens/main_app_scaffold/about_popup.dart
@@ -20,76 +20,71 @@ class AboutPopup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final contentColor = AppColors.of(context).onContainer;
+    final isWideFormat = MediaQuery.of(context).size.width > wideScreenCutoff;
+    final Color contentColor = AppColors.of(context).onContainer;
+    final double largeFontSize = isWideFormat ? 48 : 36;
 
     return DefaultTextStyle(
-      style: TextStyle(color: contentColor),
+      style: TextStyle(
+        color: contentColor,
+        fontSize: largeFontSize,
+        letterSpacing: 2,
+      ),
       child: _SizeAwareBackgroundWrapper(
+        isWideFormat: isWideFormat,
         child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(50),
-              child: IntrinsicWidth(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 36),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Flexible(child: _buildResumeButton()),
-                          const SizedBox(width: 10),
-                          ResponsiveButton(
-                            onClicked: Navigator.of(context).pop,
-                            child: Icon(
-                              Icons.close_rounded,
-                              size: 36,
-                              color: AppColors.of(context).onContainer,
-                            ),
-                          ),
-                        ],
+            IntrinsicWidth(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (isWideFormat)
+                    _buildCombinedResumeAndCloseButtons(context, contentColor)
+                  else
+                    ..._buildStackedResumeAndCloseButtons(
+                      context,
+                      contentColor,
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 36),
+                    child: _buildEmailButton(contentColor, largeFontSize),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 36),
+                    child: _buildGithubButton(contentColor, largeFontSize),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 36),
+                    child: _buildDevpostButton(contentColor, largeFontSize),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      bottom: isWideFormat ? 50 : 0,
+                    ),
+                    child: _buildShadertoyButton(contentColor, largeFontSize),
+                  ),
+                  if (!isWideFormat) const Spacer(),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 5),
+                    child: Text(
+                      'Created with Flutter, GLSL, and lots of coffee in my Los Angeles apartment.',
+                      style: TextStyle(
+                        fontSize: 16,
+                        letterSpacing: 2,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 36),
-                      child: _buildEmailButton(contentColor),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 36),
-                      child: _buildGithubButton(contentColor),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 36),
-                      child: _buildDevpostButton(contentColor),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 50),
-                      child: _buildShadertoyButton(contentColor),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 5),
-                      child: Text(
-                        'Created with Flutter, GLSL, and lots of coffee in my Los Angeles apartment.',
-                        style: TextStyle(
-                          fontSize: 16,
-                          letterSpacing: 2,
-                        ),
-                      ),
-                    ),
-                    _buildSourceMessage(),
-                  ]
-                      .animate(interval: 75.ms)
-                      .slideY(
-                        begin: 0.2,
-                        end: 0,
-                        duration: 350.ms,
-                        curve: Curves.easeOutCubic,
-                      )
-                      .fadeIn(),
-                ),
+                  ),
+                  _buildSourceMessage(),
+                ]
+                    .animate(interval: 75.ms)
+                    .slideY(
+                      begin: 0.2,
+                      end: 0,
+                      duration: 350.ms,
+                      curve: Curves.easeOutCubic,
+                    )
+                    .fadeIn(),
               ),
             ),
           ],
@@ -98,34 +93,22 @@ class AboutPopup extends StatelessWidget {
     );
   }
 
-  Widget _buildResumeButton() {
-    return ResponsiveButton(
-      onClicked: _downloadResume,
-      child: const Text(
-        'Download Resumé',
-        style: TextStyle(
-          fontSize: 48,
-          letterSpacing: 2,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildEmailButton(Color color) {
-    return ResponsiveButton(
-      onClicked: () => Clipboard.setData(
-        const ClipboardData(text: 'benjaminweschler@gmail.com'),
-      ),
+  Widget _buildCombinedResumeAndCloseButtons(
+      BuildContext context, Color color) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 36),
       child: Row(
-        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Icon(Icons.email_outlined, size: 48, color: color),
-          const SizedBox(width: 20),
-          const Text(
-            'Copy Email',
-            style: TextStyle(
-              fontSize: 48,
-              letterSpacing: 2,
+          Flexible(child: _buildResumeButton()),
+          const SizedBox(width: 10),
+          ResponsiveButton(
+            onClicked: Navigator.of(context).pop,
+            child: Icon(
+              Icons.close_rounded,
+              size: 36,
+              color: color,
             ),
           ),
         ],
@@ -133,7 +116,56 @@ class AboutPopup extends StatelessWidget {
     );
   }
 
-  Widget _buildGithubButton(Color color) {
+  List<Widget> _buildStackedResumeAndCloseButtons(
+      BuildContext context, Color color) {
+    return [
+      Padding(
+        padding: const EdgeInsets.only(bottom: 36),
+        child: Align(
+          alignment: Alignment.topRight,
+          child: ResponsiveButton(
+            onClicked: Navigator.of(context).pop,
+            child: Icon(
+              Icons.close_rounded,
+              size: 36,
+              color: color,
+            ),
+          ),
+        ),
+      ),
+      Flexible(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 36),
+          child: _buildResumeButton(),
+        ),
+      ),
+    ];
+  }
+
+  Widget _buildResumeButton() {
+    return ResponsiveButton(
+      onClicked: _downloadResume,
+      child: const Text('Download Resumé'),
+    );
+  }
+
+  Widget _buildEmailButton(Color color, double size) {
+    return ResponsiveButton(
+      onClicked: () => Clipboard.setData(
+        const ClipboardData(text: 'benjaminweschler@gmail.com'),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.email_outlined, size: size, color: color),
+          const SizedBox(width: 20),
+          const Flexible(child: Text('Copy Email')),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildGithubButton(Color color, double size) {
     return ResponsiveButton(
       onClicked: () => launchUrl(
         Uri.parse('https://www.github.com/benweschler'),
@@ -144,22 +176,16 @@ class AboutPopup extends StatelessWidget {
           Image.asset(
             'assets/github-invertocat-logo.png',
             color: color,
-            height: 48,
+            height: size,
           ),
           const SizedBox(width: 20),
-          const Text(
-            'GitHub',
-            style: TextStyle(
-              fontSize: 48,
-              letterSpacing: 2,
-            ),
-          ),
+          const Text('GitHub'),
         ],
       ),
     );
   }
 
-  Widget _buildDevpostButton(Color color) {
+  Widget _buildDevpostButton(Color color, double size) {
     return ResponsiveButton(
       onClicked: () => launchUrl(
         Uri.parse('https://devpost.com/benjaminweschler'),
@@ -170,22 +196,16 @@ class AboutPopup extends StatelessWidget {
           Image.asset(
             'assets/devpost-logo.png',
             color: color,
-            height: 48,
+            height: size,
           ),
           const SizedBox(width: 20),
-          const Text(
-            'Devpost',
-            style: TextStyle(
-              fontSize: 48,
-              letterSpacing: 2,
-            ),
-          ),
+          const Text('Devpost'),
         ],
       ),
     );
   }
 
-  Widget _buildShadertoyButton(Color color) {
+  Widget _buildShadertoyButton(Color color, double size) {
     return ResponsiveButton(
       onClicked: () => launchUrl(
         Uri.parse('https://www.shadertoy.com/user/welches'),
@@ -196,16 +216,10 @@ class AboutPopup extends StatelessWidget {
           Image.asset(
             'assets/shadertoy-logo.png',
             color: color,
-            height: 48,
+            height: size,
           ),
           const SizedBox(width: 20),
-          const Text(
-            'Shadertoy',
-            style: TextStyle(
-              fontSize: 48,
-              letterSpacing: 2,
-            ),
-          ),
+          const Text('Shadertoy'),
         ],
       ),
     );
@@ -215,9 +229,11 @@ class AboutPopup extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        const Text(
-          'View this website\'s source code ',
-          style: TextStyle(fontSize: 16, letterSpacing: 2),
+        const Flexible(
+          child: Text(
+            'View this website\'s source code ',
+            style: TextStyle(fontSize: 16, letterSpacing: 2),
+          ),
         ),
         ResponsiveButton(
           onClicked: () => launchUrl(
@@ -242,28 +258,33 @@ class AboutPopup extends StatelessWidget {
 }
 
 class _SizeAwareBackgroundWrapper extends StatelessWidget {
+  final bool isWideFormat;
   final Widget child;
 
-  const _SizeAwareBackgroundWrapper({required this.child});
+  const _SizeAwareBackgroundWrapper({
+    required this.isWideFormat,
+    required this.child,
+  });
 
   @override
   Widget build(BuildContext context) {
-    if(MediaQuery.of(context).size.width < wideScreenCutoff) {
+    if (isWideFormat) {
       return Container(
-        color: AppColors.of(context).container,
-        width: double.infinity,
-        height: double.infinity,
+        padding: const EdgeInsets.all(50),
+        decoration: BoxDecoration(
+          color: AppColors.of(context).container,
+          borderRadius: BorderRadius.circular(10),
+        ),
         child: child,
       );
     }
 
     return Container(
-      decoration: BoxDecoration(
-        color: AppColors.of(context).container,
-        borderRadius: BorderRadius.circular(10),
-      ),
+      padding: const EdgeInsets.all(35),
+      color: AppColors.of(context).container,
+      width: double.infinity,
+      height: double.infinity,
       child: child,
     );
   }
 }
-
