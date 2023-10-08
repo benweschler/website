087f2e97596056d0c5fffeb56eb0e47896155263
Ben Weschler <benjaminweschler@gmail.com>
2023-10-08 10:50:09 -0700

    Added works on mobile prompt.

--- lib/screens/initialization_screen.dart
+++ lib/screens/initialization_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:website/constants.dart';
 import 'package:website/style/theme.dart';
 import 'package:website/widgets/linear_loading_indicator.dart';
 
@@ -9,35 +12,58 @@ class InitializationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final showWorksOnMobilePrompt = !(MediaQuery
+        .of(context)
+        .size
+        .width < wideScreenCutoff) && !_isMobileBrowser();
+
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Expanded(
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  'Loading',
-                  style: TextStyle(
-                    fontSize: 16,
-                    height: 1.25,
-                    letterSpacing: 1.33,
-                  ),
+            const Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'Loading',
+                style: TextStyle(
+                  fontSize: 16,
+                  height: 1.25,
+                  letterSpacing: 1.33,
                 ),
               ),
             ),
             const SizedBox(height: 15),
-            Expanded(
+            SizedBox(
+              height: 100,
               child: LinearLoadingIndicator(
                 progress: loadingPercentage / 100,
                 color:
-                    AppColors.of(context).isDark ? Colors.white : Colors.black,
+                AppColors
+                    .of(context)
+                    .isDark ? Colors.white : Colors.black,
               ),
             ),
+            if (showWorksOnMobilePrompt)
+              const Text(
+                'This webapp also works on mobile',
+                style: TextStyle(
+                  fontSize: 14,
+                  height: 1.25,
+                  letterSpacing: 1.33,
+                ),
+              )
           ],
         ),
       ),
     );
   }
+}
+
+bool _isMobileBrowser() {
+  final userAgent = window.navigator.userAgent;
+  return userAgent.contains('Mobile') ||
+      userAgent.contains('Tablet') ||
+      userAgent.contains('Android') ||
+      userAgent.contains('iOS');
 }
